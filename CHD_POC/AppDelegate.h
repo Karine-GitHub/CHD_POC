dc2ee9269e22205b7953c173841a7666b47eabab
admin <admin@admins-Mac-mini.local>
2014-09-22 12:05:52 +0200

    Initial Commit

---
+++ CHD_POC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CHD_POC
+//
+//  Created by admin on 9/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CHD_POCTests/CHD_POCTests.m
@@ -0,0 +1,34 @@
+//
+//  CHD_POCTests.m
+//  CHD_POCTests
+//
+//  Created by admin on 9/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CHD_POCTests : XCTestCase
+
+@end
+
+@implementation CHD_POCTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
